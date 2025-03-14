740bbd92726564364d6a62290ed59206c3416311
Valentin Valkanov <valentinvalkanof@gmail.com>
2025-03-14 15:56:47 +0200

    The basic of Routing in Laravel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function (){
+   return 'Welcome to Dashboard';
+});
+
+Route::get('/users', function (){
+    return ['Gio', 'Val'];
+});
+
+Route::redirect('/', '/dashboard');
